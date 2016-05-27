b454210c2be21e08891e43d94b982e5ccfe75b2c
JahnChoi <tbajohn@gmail.com>
2016-05-27 08:20:13 -0700

    Add files via upload

--- EndGame.java
+++ EndGame.java
@@ -1,4 +1,20 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 public class EndGame
 {
     //makes new panel displaying a cheerful messge of victory
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
+    private JButton button;
+    
+    public EndGame( String[] teamNames )
+    {
+        
+        frame = new JFrame();
+        frame.setTitle( "Team " +  );
+    }
 }
